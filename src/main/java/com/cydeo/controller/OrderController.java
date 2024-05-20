7ad0910b887d99cb27e58032e43673270ff448b7
enesorhan <enestcp@gmail.com>
2024-05-20 05:45:21 +0200

    fixed the bugs and errors in the app, now it's running as it is supposed to

--- src/main/java/com/cydeo/controller/OrderController.java
+++ src/main/java/com/cydeo/controller/OrderController.java
@@ -20,12 +20,12 @@ public class OrderController {
     }
 
     @GetMapping("/current")
-    public String orderForm(UUID pizzaId, Model model) {
+    public String orderForm(@RequestParam UUID pizzaId, Model model) throws Exception {
 
         PizzaOrder pizzaOrder = new PizzaOrder();
 
         // TODO fix the getPizza() method below in line 46.
-        pizzaOrder.setPizza(getPizza(pizzaId));
+        pizzaOrder.setPizza(getPizza(pizzaId)); //so here we are setting and getting the pizzaId of the pizza we chose to order, jumping to line 39
 
         model.addAttribute("pizzaOrder", pizzaOrder);
 
@@ -33,19 +33,20 @@ public class OrderController {
     }
 
     @PostMapping("/{pizzaId}")
-    public String processOrder(UUID pizzaId, @ModelAttribute PizzaOrder pizzaOrder) {
+    public String processOrder(@PathVariable UUID pizzaId, @ModelAttribute PizzaOrder pizzaOrder) throws Exception {
 
         // Saving the order
-        pizzaOrder.setPizza(getPizza(pizzaId));
+        pizzaOrder.setPizza(getPizza(pizzaId)); // the reason why we are resetting and getting the pizzaId here after submitting the pizza is to not lose the pizza id.
         System.out.println("Order is successfully saved");
 
         return "redirect:/home";
     }
 
     //TODO complete method
-    private Pizza getPizza(UUID pizzaId) {
+    private Pizza getPizza(UUID pizzaId) throws Exception {
         // Get the pizza from repository based on it's id
-        return new Pizza();
+      return  pizzaRepository.readAll().stream().filter(p -> p.getId().equals(pizzaId)).findFirst().orElseThrow(() -> new Exception("Pizza not found"));
+
     }
 
 }
